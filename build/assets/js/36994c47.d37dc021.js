37d4149ab25fe4db105fedb25758da209633b511
Ferit Kahraman <feritkahraman32135@gmail.com>
2025-03-21 14:36:12 +0330

    chore: Remove build directory from Git tracking

--- build/assets/js/36994c47.d37dc021.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkryvit_docs=self.webpackChunkryvit_docs||[]).push([[9858],{5516:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
--- build/assets/js/a7456010.f8b5e442.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkryvit_docs=self.webpackChunkryvit_docs||[]).push([[1235],{8552:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
